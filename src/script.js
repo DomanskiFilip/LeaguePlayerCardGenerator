c3b9753257658e3d26ddc5d22fb563c09184dc8d
CasualStrategy <filip_domanski@outlook.com>
2024-06-12 16:15:48 +0200

    back to functional state

--- src/script.js
+++ src/script.js
@@ -11,7 +11,7 @@ function checkForm() {
         document.getElementById("FillFormError").innerHTML = "PLEASE FILL THE FORM!";
     } else {
         showCard();
-        GetplayerPuuid(server, serversRitoID, summonerName, tag, apiKey);    
+        getPlayerPuuid(server, serversRitoID, summonerName, tag, apiKey);    
     }
 }
 
@@ -23,70 +23,33 @@ function showCard() {
 }
 
 
-async function GetplayerPuuid(server, serversRitoID, summonerName, tag, apiKey) {
-    const url = "https://" + serversRitoID + ".api.riotgames.com/riot/account/v1/accounts/by-riot-id/"+ summonerName +"/"+ tag +"?api_key=" + apiKey;
-    try {
+async function getPlayerPuuid(server, serversRitoID, summonerName, tag, apikey) {
+    const url = "http://localhost:3000/getPlayerPuuid?serversRitoID=" + serversRitoID + "&summonerName=" + summonerName + "&tag=" + tag + "&server=" + server + "&apikey=" + apikey;  try {
         const request = await fetch(url);
-        if (!request.ok) { // check if HTTP status is 2xx
+        if (!request.ok) {
             throw new Error(`HTTP error! status: ${request.status}`);
         }
-        let data = await request.json();
-        getSummonerId(server, apiKey, data.puuid);
+        let rankInfo = await request.json();
+        fillRankInfo(rankInfo);
     } catch (error) {
         console.log(error);
     }
 }
 
-//Function getSummonerId fills riot api url with server id, player puuid and user Api Key and returns encripted summoner id.
-async function getSummonerId(server, apiKey, puuid) {  
-    const url = "https://" + server + ".api.riotgames.com/lol/summoner/v4/summoners/by-puuid/" + puuid + "?api_key=" + apiKey;
-    const request = await fetch(url);
-    let data = await request.json();
-    document.getElementById("summonerId").innerHTML = data.id;
-    document.getElementById("summonerLevel").innerHTML = data.summonerLevel;
-    getRankInfo(server, apiKey);
-    getTftInfo(server, apiKey);
-}
-    
 
-
-
-// Function GetRankInfo fills riot api url with server id, summoner id and user Api Key and returns summoner rank, tier, league points, wins and looses.
-async function getRankInfo(server, apiKey) {
-    let summonerId = document.getElementById("summonerId").innerHTML;
-    const url = "https://" + server + ".api.riotgames.com/lol/league/v4/entries/by-summoner/" + summonerId + "?api_key=" + apiKey;
-    const request = await fetch(url);
-    let rankData = await request.json();
+async function fillRankInfo(rankInfo) {
+    document.getElementById("summonerLevel").innerHTML = rankInfo.summonerLevel;
     // data from solo duo ranked queue
-    document.getElementById("summonerRankSolo").innerHTML = rankData[2].tier + " " + rankData[2].rank;
-    document.getElementById("summonerLeaguePointsSolo").innerHTML = rankData[2].leaguePoints;
-    document.getElementById("summonerWins&LoosesSolo").innerHTML = rankData[2].wins + " / " + rankData[2].losses;
-    document.getElementById("WinRatioSolo").innerHTML = (Math.round((rankData[2].wins / rankData[2].losses)* 100) / 100).toFixed(2);
-    // data from flex ranked queue
-    document.getElementById("summonerRankFlex").innerHTML = rankData[1].tier + " " + rankData[1].rank;
-    document.getElementById("summonerLeaguePointsFlex").innerHTML = rankData[1].leaguePoints;
-    document.getElementById("summonerWins&LoosesFlex").innerHTML = rankData[1].wins + " / " + rankData[1].losses;
-    document.getElementById("WinRatioFlex").innerHTML = (Math.round((rankData[1].wins / rankData[1].losses)* 100) / 100).toFixed(2);
-    console.log(rankData);
-}
-
-// Function GetTftInfo fills riot api url with server id, summoner id and user Api Key and returns summoner rank, tier, league points, wins and looses in TFT.
-async function getTftInfo(server, apiKey) {
-    let summonerId = document.getElementById("summonerId").innerHTML;
-    const url = "https://" + server + ".api.riotgames.com/tft/league/v1/entries/by-summoner/" + summonerId + "?api_key=" + apiKey;
-    const request = await fetch(url);
-    let tftData = await request.json();
-    //data from double up queue
-    document.getElementById("summonerRankTftDoubleUp").innerHTML = tftData[0].tier + " " + tftData[0].rank;
-    document.getElementById("summonerLeaguePointsTftDoubleUp").innerHTML = tftData[0].leaguePoints;
-    document.getElementById("summonerWins&LoosesTftDoubleUp").innerHTML = tftData[0].wins + " / " + tftData[0].losses;
-    document.getElementById("WinRatioTftDoubleUp").innerHTML = (Math.round((tftData[0].wins / tftData[0].losses)* 100) / 100).toFixed(2);
-    //data from tft ranked queue
-    document.getElementById("summonerRankTft").innerHTML = tftData[1].tier + " " + tftData[1].rank;
-    document.getElementById("summonerLeaguePointsTft").innerHTML = tftData[1].leaguePoints;
-    document.getElementById("summonerWins&LoosesTft").innerHTML = tftData[1].wins + " / " + tftData[1].losses;
-    document.getElementById("WinRatioTft").innerHTML = (Math.round((tftData[1].wins / tftData[1].losses)* 100) / 100).toFixed(2);
-    console.log(tftData);
+    document.getElementById("summonerRankSolo").innerHTML = rankInfo.tier + " " + rankInfo.rank;
+    document.getElementById("summonerLeaguePointsSolo").innerHTML = rankInfo.leaguePoints;
+    document.getElementById("summonerWins&LoosesSolo").innerHTML = rankInfo.wins + " / " + rankInfo.losses;
+    document.getElementById("WinRatioSolo").innerHTML = (Math.round((rankInfo.wins / rankInfo.losses)* 100) / 100).toFixed(2);
+    // data from tft double up ranked queue
+    document.getElementById("summonerRankTftDoubleUp").innerHTML = rankInfo.tierDoubleUp + " " + rankInfo.rankDoubleUp;
+    document.getElementById("summonerLeaguePointsTftDoubleUp").innerHTML = rankInfo.leaguePointsDoubleUp;
+    document.getElementById("summonerWins&LoosesTftDoubleUp").innerHTML = rankInfo.winsDoubleUp + " / " + rankInfo.lossesDoubleUp;
+    document.getElementById("WinRatioTftDoubleUp").innerHTML = (Math.round((rankInfo.winsDoubleUp / rankInfo.lossesDoubleUp)* 100) / 100).toFixed(2);
+    console.log(rankInfo);
 }
 
 // Function clears form and clears/hiddes card if needed.
@@ -94,16 +57,14 @@ function clearForm() {
     //clears form
     document.getElementById("summonerName").value = "";
     document.getElementById("server").value = "";
+    document.getElementById("serverRitoID").value = "";
+    document.getElementById("tag").value = "";
     //clears card
     if (document.getElementById("summoner").value != "") {
     document.getElementById("summonerRankSolo").innerHTML = "";
     document.getElementById("summonerLeaguePointsSolo").innerHTML = "";
     document.getElementById("summonerWins&LoosesSolo").innerHTML = "";
     document.getElementById("WinRatioSolo").innerHTML = "";
-    document.getElementById("summonerRankFlex").innerHTML = "";
-    document.getElementById("summonerLeaguePointsFlex").innerHTML = "";
-    document.getElementById("summonerWins&LoosesFlex").innerHTML = "";
-    document.getElementById("WinRatioFlex").innerHTML = "";
     document.getElementById("summonerRankTftDoubleUp").innerHTML = "";
     document.getElementById("summonerLeaguePointsTftDoubleUp").innerHTML = "";
     document.getElementById("summonerWins&LoosesTftDoubleUp").innerHTML = "";
---
+++ src/server.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var cors = require('cors');
+const axios = require('axios');
+
+
+var app = express();
+
+app.use(cors());
+
+async function getPlayerPuuid(serversRitoID, summonerName, tag, API_Key) {
+    const url = "https://" + serversRitoID + ".api.riotgames.com/riot/account/v1/accounts/by-riot-id/" + summonerName + "/" + tag + "?api_key=" + API_Key;
+    try {
+        const response = await axios.get(url);
+        console.log(response.data);
+        return response.data.puuid;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getSummonerInfo(server, puuid, API_Key) {
+    const url = "https://" + server + ".api.riotgames.com/lol/summoner/v4/summoners/by-puuid/" + puuid + "?api_key=" + API_Key;
+    try {
+        const response = await axios.get(url);
+        console.log(response.data);
+        return { id: response.data.id, summonerLevel: response.data.summonerLevel };
+    } catch (error) {
+        console.error(error);
+        return { id: null, summonerLevel: null }; // return null values in case of an error
+    }
+}
+
+async function getRankInfo(server, summonerId, API_Key) {
+    const url = "https://" + server + ".api.riotgames.com/lol/league/v4/entries/by-summoner/" + summonerId + "?api_key=" + API_Key;
+    try {
+        const response = await axios.get(url);
+        console.log(response.data);
+        return { tier: response.data[0].tier, rank: response.data[0].rank, leaguePoints: response.data[0].leaguePoints, wins: response.data[0].wins, losses: response.data[0].losses };
+    } catch (error) {
+        console.error(error);
+        return { tier: null, rank: null, leaguePoints: null, wins: null, losses: null }; // return null values in case of an error
+    }
+}
+
+
+async function getTftInfo(server, summonerId, API_Key){
+    const url = "https://" + server + ".api.riotgames.com/tft/league/v1/entries/by-summoner/" + summonerId + "?api_key=" + API_Key;
+    try {
+        const response = await axios.get(url);
+        console.log(response.data);
+        return { tierDoubleUp: response.data[0].tier, rankDoubleUp: response.data[0].rank, leaguePointsDoubleUp: response.data[0].leaguePoints, winsDoubleUp: response.data[0].wins, lossesDoubleUp: response.data[0].losses };
+    } catch (error) {
+        console.error(error);
+        return { tierDoubleUp: null, rankDoubleUp: null, leaguePointsDoubleUp: null, winsDoubleUp: null, lossesDoubleUp: null }; // return null values in case of an error
+    }
+}
+
+app.get('/getPlayerPuuid', async (req, res) => {
+    const PUUID = await getPlayerPuuid(req.query.serversRitoID, req.query.summonerName, req.query.tag, req.query.apikey);
+    const summonerInfo = await getSummonerInfo(req.query.server, PUUID, req.query.apikey);
+    const rankInfo = await getRankInfo(req.query.server, summonerInfo.id, req.query.apikey);
+    const tftInfo = await getTftInfo(req.query.server, summonerInfo.id, req.query.apikey);
+    res.send({ 
+        rank: rankInfo.rank,
+        tier: rankInfo.tier,
+        leaguePoints: rankInfo.leaguePoints,
+        wins: rankInfo.wins,
+        losses: rankInfo.losses,
+
+        rankDoubleUp: tftInfo.rankDoubleUp, 
+        tierDoubleUp: tftInfo.tierDoubleUp,
+        leaguePointsDoubleUp: tftInfo.leaguePointsDoubleUp,
+        winsDoubleUp: tftInfo.winsDoubleUp,
+        lossesDoubleUp: tftInfo.lossesDoubleUp,
+
+        summonerLevel: summonerInfo.summonerLevel
+    });
+});
+
+app.listen(3000, function () {
+    console.log('CORS-enabled web server listening on port 3000');
+});
